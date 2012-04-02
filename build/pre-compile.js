7f05128dab0d84e0fd079e04f76d5b6b0c2f2e2d
Wei Kin Huang <weikin.huang04@gmail.com>
2012-04-02 14:42:40 -0400

    moving to node based build process

--- build/pre-compile.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var print = require(/^v0\.[012]/.test(process.version) ? "sys" : "util").print,
-	src = require("fs").readFileSync("/dev/stdin", "utf8").toString();
-
-print( src );
