1b000dc77adcf99ea6d579cd86ecb255f351b189
Wei Kin Huang <weikin.huang04@gmail.com>
2012-04-19 17:13:39 -0400

    Add npm module files

---
+++ index.js
@@ -0,0 +1,6 @@
+/*!
+ * classify-node
+ * Copyright(c) 2011-2012 Wei Kin Huang
+ * MIT Licensed
+ */
+module.exports = require('./dist/classify.js');
---
+++ package.js
@@ -0,0 +1,12 @@
+{
+	"name" : "classify",
+	"description" : "Classify is a Javascript library that allows for cross platform classical inheritance and namespaces behind the prototype syntax in an easy to use interface function.",
+	"keywords" : [ "util", "functional", "server", "client", "browser", "prototype", "object-oriented", "class", "classes", "inheritance", "abstraction" ],
+	"author" : "Wei Kin Huang <wei@closedinterval.com>",
+	"version" : "0.7.5",
+	"repository" : {
+		"type" : "git",
+		"url" : "https://github.com/weikinhuang/Classify.git"
+	},
+	"main" : "dist/classify"
+};
