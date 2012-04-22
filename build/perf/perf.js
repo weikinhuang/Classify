a3233ed699493de76188e69c191a3907e0affcfc
Wei Kin Huang <weikin.huang04@gmail.com>
2012-04-22 16:06:14 -0400

    Adding ability to add benchmarking tests to the build process and updating 3rd party libs

---
+++ build/perf/perf.js
@@ -0,0 +1,78 @@
+(function(window, tests) {
+
+	function addListener(element, eventName, handler) {
+		if (typeof element.addEventListener != 'undefined') {
+			element.addEventListener(eventName, handler, false);
+		} else if (typeof element.attachEvent != 'undefined') {
+			element.attachEvent('on' + eventName, handler);
+		}
+		return element;
+	}
+
+	var table = document.getElementById("perf-tests");
+	var statusElem = document.getElementById("perf-status");
+
+	tests.on("add", function(e) {
+		var test = e.target;
+		var tr = document.createElement("tr");
+		var tdName = document.createElement("th");
+		var tdBody = document.createElement("td");
+		var tdStatus = document.createElement("td");
+		var i = 0;
+
+		var preBody = document.createElement("pre");
+		tdBody.appendChild(preBody);
+
+		var aName = document.createElement("a");
+		aName.href = "#restart";
+		tdName.appendChild(aName);
+		addListener(aName, "click", function() {
+			test.abort().reset().run({
+				async : true,
+				queue : true
+			});
+			return false;
+		});
+
+		tdName.className = "test-name";
+		tdBody.className = "test-body";
+		tdStatus.className = "test-status";
+
+		aName.innerText = test.name;
+		preBody.innerText = (test.fn.toSource ? test.fn.toSource() : test.fn.toString()).replace(/\t/g, "    ").replace(/^function\s*\(.*\)\s*\{\s*[\n\r]+/, "").replace(/\s*[\n\r]+}\s*$/, "").replace(/^\    /gm, "");
+		tdStatus.innerText = "Ready";
+
+		tr.appendChild(tdName);
+		tr.appendChild(tdStatus);
+		tr.appendChild(tdBody);
+		table.appendChild(tr);
+
+		// bind events
+		test.on("start", function(e) {
+			tr.className = "";
+			statusElem.innerText = "Starting: " + this.name;
+			tdStatus.innerText = "Running...";
+			i = 0;
+		}).on("cycle", function(e) {
+			var size = ++i;
+			statusElem.innerHTML = this.name + " &times; " + Benchmark.formatNumber(this.count) + " (" + size + " sample" + (size == 1 ? "" : "s") + ")";
+		}).on("error", function(e) {
+			tr.className = "test-error";
+		}).on("reset", function(e) {
+			tdStatus.innerText = "Ready";
+		}).on("complete", function(e) {
+			if (this.error) {
+				tdStatus.innerHTML = this.error.message;
+				return;
+			}
+			tdStatus.title = "Ran " + Benchmark.formatNumber(this.count) + " times in " + this.times.cycle.toFixed(3) + " seconds.";
+			tdStatus.innerHTML = Benchmark.formatNumber(this.hz.toFixed(this.hz < 100 ? 2 : 0)) + " <small>(&plusmn;" + this.stats.rme.toFixed(2) + "%)</small>";
+		});
+	});
+
+	tests.on("complete", function() {
+		statusElem.innerText = "All tests completed!";
+	}).on("reset", function() {
+		statusElem.innerText = "Initializing...";
+	});
+})(this, ___benchmarks);
