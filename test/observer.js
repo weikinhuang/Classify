17032b12902c7d6bb90efd0bcce4eb711bf28a34
Wei Kin Huang <weikin.huang04@gmail.com>
2012-05-02 08:56:14 -0400

    Adding unit tests for observers called with applicate

--- test/observer.js
+++ test/observer.js
@@ -424,6 +424,51 @@ QUnit.test("instantiating observer as part of object initialization", function()
 	QUnit.ok(testinstance.x !== testinstance2.x, "Separate instances of classes have different observers.");
 });
 
+QUnit.test("instantiating observer as part of object initialization with applicate", function() {
+	QUnit.expect(7);
+	var test = create({
+		__observable_x : 10,
+		__observable_y : {
+			value : 100,
+			get : function(value) {
+				return value * 10;
+			},
+			set : function(value, original) {
+				return this.a();
+			}
+		},
+		__observable_z : 1000,
+		a : function() {
+			return 1;
+		},
+		b : function(value) {
+			QUnit.equal(value, 1, "Observable event listener called internal function through applicate.");
+		},
+		e : function() {
+			return this.constructor;
+		}
+	});
+	var testinstance = test.applicate([ 2 ]);
+
+	// test that observers are created
+	QUnit.ok(testinstance.x instanceof Observer, "Applied class has instance of observer.");
+
+	QUnit.equal(testinstance.x.get(), 10, "Calling observer from object context.");
+	QUnit.equal(testinstance.y.get(), 1000, "Calling observer from object context with defined getter.");
+	QUnit.ok(testinstance.y.set() === testinstance, "The return value of setter function is a chain of parent instance.");
+	QUnit.equal(testinstance.y.get(), 10, "Calling observer from object context with setter referencing parent function.");
+
+	// add an event listener
+	testinstance.z.addListener(function(value) {
+		this.b(value);
+	});
+	testinstance.z.set(1);
+
+	// test separate instances of instantiated class
+	var testinstance2 = test.applicate([ 3 ]);
+	QUnit.ok(testinstance.x !== testinstance2.x, "Separate instances of classes have different observers.");
+});
+
 QUnit.test("instantiating observer as part of object initialization when defined in container", function() {
 	QUnit.expect(7);
 	var test = create({
